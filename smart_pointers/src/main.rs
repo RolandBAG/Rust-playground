43a14c9f065a65a1753d7252a044c362fb455bf4
James D <james@localhost.localdomain>
2024-01-25 05:56:12 +1000

    Completed automatic Drop section.

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -26,6 +26,16 @@ enum List {
     Nil,
 }
 
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) { 
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
@@ -45,6 +55,15 @@ fn main() {
     
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
-    
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+    drop(d);
+    println!("CustomSmartPointer d dropped before end of main")
 }
 
